93882ff2694cad1b53e4d0496662e04a61be73cb
Mat Sz <contact@matsz.dev>
2020-11-14 12:48:18 +0100

    added a speed counter

--- app.py
+++ app.py
@@ -1,22 +1,39 @@
 import numpy as np
 import cv2
+import threading
+import time
 
+count = 0
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-while True:
-  # Generate a grey scale bitmap.
-  float_img = np.random.random((256,256))
-  im = np.array(float_img * 255, dtype = np.uint8)
+def display_time():
+  global count
 
-  faces = face_cascade.detectMultiScale(im, 1.1, 4)
+  while True:
+    print('Current speed: ', count, ' images/s')
+    count = 0
+    time.sleep(1)
 
-  if len(faces) > 0:
-    # Draw the rectangle around each face.
-    for (x, y, w, h) in faces:
-        cv2.rectangle(im, (x, y), (x+w, y+h), (255, 0, 0), 2)
+def search_for_faces():
+  global count
 
-    # Display image.
-    cv2.imshow('img', im)
-    cv2.waitKey(0) 
-    cv2.destroyAllWindows() 
-  
+  while True:
+    # Generate a grey scale bitmap.
+    float_img = np.random.random((256,256))
+    im = np.array(float_img * 255, dtype = np.uint8)
+
+    faces = face_cascade.detectMultiScale(im, 1.1, 4)
+    count += 1
+
+    if len(faces) > 0:
+      # Draw the rectangle around each face.
+      for (x, y, w, h) in faces:
+          cv2.rectangle(im, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+      # Display image.
+      cv2.imshow('img', im)
+      cv2.waitKey(0) 
+      cv2.destroyAllWindows()
+
+threading.Thread(target=display_time).start()
+threading.Thread(target=search_for_faces).start()
